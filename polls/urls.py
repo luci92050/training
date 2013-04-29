c41965e1852f82a2d88fae76ffba86cd8ffd5694
stres <stres@stres-P4M90-M7A.(none)>
2013-04-29 12:59:33 +0300

    part6

--- polls/urls.py
+++ polls/urls.py
@@ -18,21 +18,23 @@ from polls.models import Poll
 from django.utils import timezone
 
 urlpatterns = patterns('',
-
+    url(r'^$',
+    ListView.as_view(
+        queryset=Poll.objects.filter(pub_date__lte=timezone.now) \
+            .order_by('-pub_date')[:5],
+        context_object_name='latest_poll_list',
+        template_name='polls/index.html'),
+    name='index'),
+    
     url(r'^(?P<pk>\d+)/$',
         DetailView.as_view(
             queryset=Poll.objects.filter(pub_date__lte=timezone.now),
             model=Poll,
             template_name='polls/detail.html'),
         name='detail'),
-
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='polls/detail.html'),
-        name='detail'),
     url(r'^(?P<pk>\d+)/results/$',
         DetailView.as_view(
+            queryset=Poll.objects.filter(pub_date__lte=timezone.now),
             model=Poll,
             template_name='polls/results.html'),
         name='results'),
